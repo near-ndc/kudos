46cbb8680ba8317280f3ed0b8d77193e526717e8
ZelionD <zeliondragon@gmail.com>
2023-07-25 01:13:58 +0300

    fmt fix

--- src/misc.rs
+++ src/misc.rs
@@ -2,7 +2,7 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 
 /// This type represents this contract state
-/// 
+///
 /// Public methods are available only while this contract is in [`Running`](RunningState::Running) state
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Eq, PartialEq, Clone)]
 #[serde(crate = "near_sdk::serde")]
